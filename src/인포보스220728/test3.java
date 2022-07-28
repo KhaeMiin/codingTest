a0b382c03ed0583bcf2c32912cd06756a59eab31
minMin <goals1005@naver.com>
2022-07-28 21:49:10 +0900

    코딩테스트

---
+++ src/인포보스220728/test3.java
@@ -0,0 +1,30 @@
+package 인포보스220728;
+
+
+import java.util.Arrays;
+
+/**
+ * 인포보스 문제2
+ */
+public class test3 {
+
+    public static void main(String[] args) {
+        Solution2 solution = new Solution2();
+        int[] waiting = {1, 5, 8, 2, 10, 5, 4, 6, 4, 8};
+        int[] solution1 = solution.solution(waiting);
+        for (int i : solution1) {
+            System.out.println("i = " + i);
+        }
+
+    }
+}
+
+
+class Solution2 {
+    public int[] solution(int[] waiting) {
+        int[] answer = Arrays.stream(waiting).distinct().toArray();
+
+
+        return answer;
+    }
+}
